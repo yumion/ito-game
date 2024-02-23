856aa4015297c3575a42ec657be92a35b35a2ef6
yumion <a.yamada0703@gmail.com>
2024-02-23 21:53:06 +0900

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# ito-game
+
+### start game
+```
+localhost:5000/reset
+```
+
+### get your number
+```
+localhost:5000/get
+```
+
---
+++ src/app.py
@@ -0,0 +1,39 @@
+import random
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+numbers_list = []
+
+
+def generate_numbers(max_number: int = 100):
+    numbers_list = list(range(1, max_number + 1))
+    random.shuffle(numbers_list)
+    return numbers_list
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return jsonify({"reset game": "/reset", "get your number": "/get"}), 200
+
+
+@app.route("/reset", methods=["GET"])
+def reset():
+    global numbers_list
+    max_number = int(request.args.get("max", 100))
+    numbers_list = generate_numbers(max_number)
+    return jsonify({"message": f"reset: max {max_number}"}), 200
+
+
+@app.route("/get", methods=["GET"])
+def get():
+    global numbers_list
+    if not numbers_list:
+        return jsonify({"message": "No more numbers. Please reset."}), 200
+    your_number = numbers_list.pop()
+    return jsonify({"number": your_number}), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
